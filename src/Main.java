634140edfee6afcbd77f03d54f582e0b7f73a8a6
karinapavlova <karinap223@gmail.com>
2023-03-03 21:28:38 +0200

    introducing other data types: double, boolean, String

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,24 @@ public class Main {
        myDaughterAge = 67;
 // sout + enter
         System.out.println(myDaughterAge);
+        // use camel case starting with lower letter
+        String myName = "Karina";
+        String mySurname = "Pavlova";
+        System.out.println(myName + "-" + mySurname);
+
+        String five = "5";
+        String otherFive = "5";
+        System.out.println(five+otherFive);
+
+        // true and false
+        boolean trueValue = true;
+        trueValue = false;
+
+        System.out.println(trueValue);
+
+        // 11.0
+        int eleven = 11;
+        // double type for number with fraction part
+        double trueEleven = 11.0;
     }
 }
