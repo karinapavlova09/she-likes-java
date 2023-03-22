7f62d17769616dc37060ccd70b6451b4bd9c92af
karinapavlova <karinap223@gmail.com>
2023-03-22 21:08:03 +0200

    for loop

---
+++ src/com/sda/she_likes_java/loops/while_loop/ForLoopExample.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class ForLoopExample {
+
+    public static void main(String[] args) {
+
+        for (
+                int number = 0; // initialization part - executed only once
+                number < 100; // condition of the loop
+                number++ // executed as last instruction in current iteration
+        ) {
+            System.out.println("Number value is: " + number);
+        }
+        // the same loop but with while
+        int number = 0;
+        while (number < 100) {
+            System.out.println("Number value is: " + number);
+            number++;
+        }
+    }
+}
