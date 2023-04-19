efd9b6befb22e73c287b7e2ee2b8e9b7b645ece2
karinapavlova <karinap223@gmail.com>
2023-04-19 20:13:02 +0300

    default modifier (no modifier)

---
+++ src/com/sda/she_likes_java/visibility/default_modifier/main/DefaultModifierMain.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.visibility.default_modifier.main;
+
+public class DefaultModifierMain {
+    public static void main(String[] args) {
+        // try to use Car class with default visibility
+        // I am not able to use class with package private visibility outside its package
+
+    //    Car car = new Car();
+    }
+}
