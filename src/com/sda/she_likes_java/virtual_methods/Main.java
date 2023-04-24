9441c78904374763860ee6f0d0d3f8ae0ad130b9
karinapavlova <karinap223@gmail.com>
2023-04-24 21:22:25 +0300

    Safe casting with instanceof operator

--- src/com/sda/she_likes_java/virtual_methods/Main.java
+++ src/com/sda/she_likes_java/virtual_methods/Main.java
@@ -5,5 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Mammal human = new Human();
         human.whoYouAre();
+
+        Human nullHuman = null;
+
+        HumanLife humanLife = new HumanLife();
+        humanLife.tellMeWhoYouAre(nullHuman);
+
+        Woman woman = new Woman();
+        humanLife.tellMeWhoYouAre(woman);
+
+        Man man = new Man();
+        humanLife.tellMeWhoYouAre(man);
+
+
+        System.out.println("Casting objects");
+        Human anotherWoman = new Woman();
+        Woman casterFromHuman = (Woman) anotherWoman;
+        casterFromHuman.learningJava();
+
+        Man manForSure = new Man();
+        Human anotherMan = manForSure;
+        humanLife.tryToCastHumanToMan(anotherMan);
+        humanLife.tryToCastHumanToMan(new Man());
+        humanLife.tryToCastHumanToMan(new Woman());
+
     }
 }
+
